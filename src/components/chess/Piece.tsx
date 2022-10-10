ef8697fedd09cfd289f340d091ef5571dafe70eb
daniel-escoto <escotodanny62@gmail.com>
2022-10-10 16:05:59 -0700

    component logic

---
+++ src/components/chess/Piece.tsx
@@ -0,0 +1,35 @@
+import { component$ } from "@builder.io/qwik";
+import BlackBishop from "../../../public/pieces/BlackBishop.svg";
+import BlackKing from "../../../public/pieces/BlackKing.svg";
+import BlackKnight from "../../../public/pieces/BlackKnight.svg";
+import BlackPawn from "../../../public/pieces/BlackPawn.svg";
+import BlackQueen from "../../../public/pieces/BlackQueen.svg";
+import BlackRook from "../../../public/pieces/BlackRook.svg";
+import WhiteBishop from "../../../public/pieces/WhiteBishop.svg";
+import WhiteKing from "../../../public/pieces/WhiteKing.svg";
+import WhiteKnight from "../../../public/pieces/WhiteKnight.svg";
+import WhitePawn from "../../../public/pieces/WhitePawn.svg";
+import WhiteQueen from "../../../public/pieces/WhiteQueen.svg";
+import WhiteRook from "../../../public/pieces/WhiteRook.svg";
+import { Piece, PieceType } from "../../models/Piece";
+
+function getPieceImage(piece: Piece) {
+  switch (piece.type) {
+    case PieceType.Pawn:
+      return piece.color === "white" ? WhitePawn : BlackPawn;
+    case PieceType.Knight:
+      return piece.color === "white" ? WhiteKnight : BlackKnight;
+    case PieceType.Bishop:
+      return piece.color === "white" ? WhiteBishop : BlackBishop;
+    case PieceType.Rook:
+      return piece.color === "white" ? WhiteRook : BlackRook;
+    case PieceType.Queen:
+      return piece.color === "white" ? WhiteQueen : BlackQueen;
+    case PieceType.King:
+      return piece.color === "white" ? WhiteKing : BlackKing;
+  }
+}
+
+export default component$((piece: Piece) => {
+  return <img src={getPieceImage(piece)} />;
+});
---
+++ src/components/chess/Tile.tsx
@@ -0,0 +1,15 @@
+import { component$ } from "@builder.io/qwik";
+import { Tile } from "~/models/Tile";
+import Piece from "./Piece";
+
+function getTileColor(tile: Tile) {
+  return tile.color === "white" ? "bg-white" : "bg-black";
+}
+
+export default component$((tile: Tile) => {
+  return (
+    <div className={`w-12 h-12 ${getTileColor(tile)}`}>
+      {tile.piece && <Piece color={tile.piece.color} type={tile.piece.type} />}
+    </div>
+  );
+});
